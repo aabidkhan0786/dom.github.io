a23a1f259ee94a1cd2b50fc7053e6605490fa6ba
aabidkhan0786 <62328527+aabidkhan0786@users.noreply.github.com>
2020-07-17 19:50:45 +0530

    Tic Tac Toc- An Example Of DOM
    Its a non functional game just for understanding the main concept of Document Object Model.

---
+++ index.js
@@ -0,0 +1,27 @@
+var restart=document.querySelector("#b");
+
+
+var sq = document.querySelectorAll("td");
+
+
+function clearBoard(){
+    for(var i=0;i<sq.length;i++){
+        sq[i].textContent="";
+    }
+}
+
+restart.addEventListener("click",clearBoard);
+
+function changeMarker(){
+    if(this.textContent===""){
+        this.textContent="X";
+    }else if(this.textContent==="X"){
+            this.textContent="O";
+    }else{
+            this.textContent="";
+        }
+}
+
+for(var i = 0;i<sq.length;i++){
+    sq[i].addEventListener("click",changeMarker);
+}
